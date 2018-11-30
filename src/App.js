52e3e1f978881f1449239a0520e2d02059228924
ozaki <fs65ugumori@yahoo.co.jp>
2018-12-01 03:54:18 +0900

    add blueprint

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello IPPO</h1>
+        <h1 class="bp3-heading">Hello IPPO</h1>
       </div>
     );
   }
