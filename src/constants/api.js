7e7fe6cfeea1a099e11d09f6af9bafb67f4bec67
ozaki <fs65ugumori@yahoo.co.jp>
2018-12-02 17:28:34 +0900

    add local settings

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,6 @@
-const graphqlServer = 'http://localhost:4000/graphql';
+const graphqlServer = process.env.REACT_APP_LOCAL
+  ? 'http://localhost:4000/graphql'
+  : 'https://68v4ch48mg.execute-api.ap-northeast-1.amazonaws.com/dev/graphql';
 
 export default {
   graphqlServer,
