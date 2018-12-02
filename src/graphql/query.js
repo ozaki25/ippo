26dee6d25046a3284199b2c617d0f38737cb78bd
ozaki <fs65ugumori@yahoo.co.jp>
2018-12-03 00:38:18 +0900

    update graphql query

--- src/graphql/query.js
+++ src/graphql/query.js
@@ -1,15 +1,27 @@
 import gql from 'graphql-tag';
 
 const hello = gql`
-  query Hello {
+  query {
     hello
     count
   }
 `;
 
-// const getEvents = gql`{}`;
+const connpassEvents = gql`
+  query {
+    connpass {
+      events {
+        event_id
+        title
+        event_url
+        catch
+        place
+      }
+    }
+  }
+`;
 
 export default {
   hello,
-  // getEvents,
+  connpassEvents,
 };
