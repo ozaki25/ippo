9601cb48b99be7f263aa2a943ce088349a60f159
ozaki <fs65ugumori@yahoo.co.jp>
2018-12-02 17:21:23 +0900

    update hello graphql

--- src/graphql/query.js
+++ src/graphql/query.js
@@ -3,7 +3,7 @@ import gql from 'graphql-tag';
 const hello = gql`
   query Hello {
     hello
-    number
+    count
   }
 `;
 
--- src/hoc/WithHello.js
+++ src/hoc/WithHello.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { graphql } from 'react-apollo';
 import query from 'src/graphql/query';
 
-const WithHello = ({ data: { hello, number } }) => (
-  <h3>{[...Array(number)].map(() => `${hello} `)}</h3>
+const WithHello = ({ data: { hello, count } }) => (
+  <h3>{[...Array(count)].map(() => `${hello} `)}</h3>
 );
 
 export default graphql(query.hello)(WithHello);
