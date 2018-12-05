35b8ab1992b8530bded6077be624e8276aed02ff
ozaki <fs65ugumori@yahoo.co.jp>
2018-12-06 08:02:16 +0900

    netlify function

--- functions/graphql.js
+++ functions/graphql.js
@@ -1,4 +1,4 @@
-const { ApolloServer, gql } = require('apollo-server-lambda');
+const { ApolloServer, gql } = require('apollo-server');
 const fetchConnpassEvents = require('./src/fetchConnpassEvent');
 
 const typeDefs = gql`
