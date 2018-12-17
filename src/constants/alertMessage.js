b4ee9b0e9dcd3cb16faf2c2a9060d5092da30319
ozaki <fs65ugumori@yahoo.co.jp>
2018-12-17 21:41:25 +0900

    fixed typo

--- src/constants/alertMessage.js
+++ src/constants/alertMessage.js
@@ -1,8 +1,8 @@
 const subscribeNotification = {
-  granted: '通知設定は完了済みです',
+  granted: '通知設定は完了済みです。',
   denied:
     '通知がブロックされています。通知を受け取るためにはブラウザの設定から通知を許可して下さい。',
-  unsupported: 'このブラウザはプッシュ通知に対ししておりません。',
+  unsupported: 'このブラウザはプッシュ通知た対応しておりません。',
 };
 
 export default {
