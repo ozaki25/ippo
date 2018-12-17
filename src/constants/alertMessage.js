1be86b3c5867445e18b5bfeb0b6d7f4e94f7b370
ozaki <fs65ugumori@yahoo.co.jp>
2018-12-17 20:27:55 +0900

    modify notification icon action

---
+++ src/constants/alertMessage.js
@@ -0,0 +1,10 @@
+const subscribeNotification = {
+  granted: '通知設定は完了済みです',
+  denied:
+    '通知がブロックされています。通知を受け取るためにはブラウザの設定から通知を許可して下さい。',
+  unsupported: 'このブラウザはプッシュ通知に対ししておりません。',
+};
+
+export default {
+  subscribeNotification,
+};
