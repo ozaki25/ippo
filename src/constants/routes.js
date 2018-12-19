2dfd3b85134991e786e9e36ba635984620440bf1
ozaki <fs65ugumori@yahoo.co.jp>
2018-12-19 10:12:44 +0900

    add session management hoc

---
+++ src/constants/routes.js
@@ -0,0 +1,15 @@
+const SignIn = '/signin';
+const SignUp = '/signup';
+const Menu = '/';
+const Events = '/events';
+const NewEvent = '/events/new';
+const Publish = '/notifications/publish';
+
+export default {
+  Menu,
+  Events,
+  NewEvent,
+  Publish,
+  SignIn,
+  SignUp,
+};
--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,17 @@ import Menu from 'src/hoc/WithMenu';
 import Publish from 'src/hoc/WithPublish';
 import Events from 'src/hoc/WithEvents';
 import NewEvent from 'src/hoc/WithNewEvent';
+import ROUTES from 'src/constants/routes';
 
 const Router = () => (
   <HashRouter>
     <Switch>
-      <Route path="/" exact component={Menu} />
-      <Route path="/events" exact component={Events} />
-      <Route exact path="/events/new" component={NewEvent} />
-      <Route path="/notifications/publish" exact component={Publish} />
+      {/* <Route path={ROUTES.SignIn} exact component={SignIn} />
+      <Route path={ROUTES.SignUp} exact component={SignUp} /> */}
+      <Route path={ROUTES.Menu} exact component={Menu} />
+      <Route path={ROUTES.Events} exact component={Events} />
+      <Route path={ROUTES.NewEvent} component={NewEvent} />
+      <Route path={ROUTES.Publish} exact component={Publish} />
     </Switch>
   </HashRouter>
 );
