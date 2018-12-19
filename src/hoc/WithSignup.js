4d6075737c37172afccdde5e1495ed6be627e85e
ozaki <fs65ugumori@yahoo.co.jp>
2018-12-19 13:34:19 +0900

    fixed bug

--- src/hoc/WithSignup.js
+++ src/hoc/WithSignup.js
@@ -1,6 +1,5 @@
 import { withRouter } from 'react-router-dom';
 import { withFirebase } from 'src/context/firebase';
-
-import Signup from 'src/components/pages/Signup/';
+import Signup from 'src/components/pages/Signup';
 
 export default withRouter(withFirebase(Signup));
