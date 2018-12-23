0639b152dfc0abe9b5aa84bec88daa085074778f
ozaki <fs65ugumori@yahoo.co.jp>
2018-12-23 13:26:33 +0900

    add TweetList component

---
+++ src/components/organisms/TweetList/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {} from '@blueprintjs/core';
+import styled from 'styled-components';
+import propTypes from 'prop-types';
+import Tweet from 'src/components/molecules/Tweet';
+
+const TweetList = ({ items }) => items.map(item => <Tweet key={item.id} {...item} />);
+
+TweetList.displayName = 'TweetList';
+
+TweetList.propTypes = {
+  items: propTypes.arrayOf(
+    propTypes.shape({
+      id: propTypes.string.isRequired,
+      name: propTypes.string.isRequired,
+      text: propTypes.string.isRequired,
+      time: propTypes.string.isRequired,
+    }),
+  ),
+};
+
+TweetList.defaultProps = {
+  items: [],
+};
+
+export default TweetList;
