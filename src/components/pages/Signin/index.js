2640dc4e667439b8cc3d7ab8c5fd75b6bea93a15
ozaki <fs65ugumori@yahoo.co.jp>
2018-12-24 16:09:39 +0900

    modify google auth

--- src/components/pages/Signin/index.js
+++ src/components/pages/Signin/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import propTypes from 'prop-types';
-import { Button, Classes, Dialog, Intent } from '@blueprintjs/core';
+import { Button, Intent } from '@blueprintjs/core';
 import ROUTES from 'src/constants/routes';
 import Container from 'src/components/templates/Container';
-import BasicAuthForm from 'src/components/organisms/BasicAuthForm';
+import SigninForm from 'src/components/organisms/SigninForm';
+import SignupFormDialog from 'src/components/organisms/SignupFormDialog';
 import GoogleButton from 'src/components/atoms/GoogleButton';
-
-const styles = {
-  dialog: {
-    margin: '0 15px',
-  },
-};
 
 const ButtonContainer = styled.div`
   margin-top: 10px;
@@ -24,12 +19,28 @@ const LinkContainer = styled.div`
 `;
 
 class Signin extends React.Component {
-  state = { isOpen: false };
+  state = { isOpen: false, loading: false };
 
-  async componentDidMount() {
-    const result = await this.props.firebase.auth.getRedirectResult();
-    if (result.user) this.props.history.push(ROUTES.Menu);
+  componentWillMount() {
+    const key = sessionStorage.getItem('willRedirect');
+    if (key) {
+      sessionStorage.removeItem('willRedirect');
+      this.redirectResult();
+    }
   }
+
+  redirectResult = async () => {
+    const result = await this.props.firebase.auth.getRedirectResult();
+    const authUser = result.user;
+    if (authUser) {
+      const user = {
+        id: authUser.uid,
+        name: authUser.displayName,
+      };
+      this.props.createUser({ variables: { user } });
+      this.props.history.push(ROUTES.Menu);
+    }
+  };
 
   toggleDialog = () => this.setState(prevState => ({ isOpen: !prevState.isOpen }));
 
@@ -38,13 +49,23 @@ class Signin extends React.Component {
     this.props.history.push(ROUTES.Menu);
   };
 
-  signup = async ({ data: { email, pass } }) => {
-    await this.props.firebase.doCreateUserWithEmailAndPassword(email, pass);
+  signup = async ({ data: { email, pass, name } }) => {
+    const response = await this.props.firebase.doCreateUserWithEmailAndPassword(email, pass);
+    const user = {
+      id: response.user.uid,
+      name,
+    };
+    this.props.createUser({ variables: { user } });
     this.props.history.push(ROUTES.Menu);
   };
 
   signinWithGoogle = async () => {
-    await this.props.firebase.doSignInWithGoogle().catch(e => console.log(e));
+    try {
+      sessionStorage.setItem('willRedirect', 'true');
+      this.props.firebase.doSignInWithGoogle();
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   onChange = event => this.setState({ [event.target.name]: event.target.value });
@@ -52,7 +73,7 @@ class Signin extends React.Component {
   render() {
     return (
       <Container>
-        <BasicAuthForm onSubmit={this.signin} buttonText="ログイン" />
+        <SigninForm onSubmit={this.signin} />
         <ButtonContainer>
           <GoogleButton onClick={this.signinWithGoogle} />
         </ButtonContainer>
@@ -61,17 +82,11 @@ class Signin extends React.Component {
             新規登録
           </Button>
         </LinkContainer>
-        <Dialog
-          title="ユーザ登録"
+        <SignupFormDialog
           isOpen={this.state.isOpen}
+          onSubmit={this.signup}
           onClose={this.toggleDialog}
-          style={styles.dialog}
-          isCloseButtonShown
-        >
-          <div className={Classes.DIALOG_BODY}>
-            <BasicAuthForm buttonText="登録" onSubmit={this.signup} />
-          </div>
-        </Dialog>
+        />
       </Container>
     );
   }
@@ -86,6 +101,7 @@ Signin.propTypes = {
   firebase: propTypes.shape({
     doSignInWithEmailAndPassword: propTypes.func.isRequired,
   }).isRequired,
+  createUser: propTypes.func.isRequired,
 };
 
 Signin.defaultProps = {};
