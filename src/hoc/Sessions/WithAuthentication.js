7eb8d13878b12cc5678806260ad0fbc6d6a92b68
ozaki <fs65ugumori@yahoo.co.jp>
2018-12-24 22:30:13 +0900

    modify auth user handling

--- src/hoc/Sessions/WithAuthentication.js
+++ src/hoc/Sessions/WithAuthentication.js
@@ -1,18 +1,55 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'recompose';
+import { compose as apolloCompose, graphql } from 'react-apollo';
+import query from 'src/graphql/query';
 import { withFirebase } from 'src/context/firebase';
 import { setAuthUser } from 'src/modules/session';
 
 const withAuthentication = Component => {
   class WithAuthentication extends React.Component {
+    constructor(props) {
+      super(props);
+      // LocalStorageにデータがあればログインさせる
+      this.props.onSetAuthUser(JSON.parse(localStorage.getItem('authUser')));
+    }
+
     componentDidMount() {
       this.listener = this.props.firebase.onAuthUserListener(
-        authUser => {
+        async authUser => {
+          if (this.props.authUser) return;
+
           const { uid, displayName } = authUser;
-          this.props.onSetAuthUser({ uid, displayName });
+          const user = { uid, displayName };
+
+          if (uid && displayName) {
+            // googleでログイン
+            this.props.createUser({ variables: { user } });
+            localStorage.setItem('authUser', JSON.stringify(user));
+            this.props.onSetAuthUser(user);
+          }
+          if (!displayName) {
+            const storage = JSON.parse(sessionStorage.getItem('authUser'));
+            sessionStorage.removeItem('authUser');
+
+            if (storage && storage.name) {
+              // emailで新規登録直後
+              user.displayName = storage.name;
+              this.props.createUser({ variables: { user } });
+              localStorage.setItem('authUser', JSON.stringify(user));
+              this.props.onSetAuthUser(user);
+            } else {
+              // ログイン直後
+              const {
+                data: { fetchUser },
+              } = await this.props.fetchUser({ variables: { uid } });
+              localStorage.setItem('authUser', JSON.stringify(fetchUser));
+              this.props.onSetAuthUser({ uid: fetchUser.uid, displayName: fetchUser.displayName });
+            }
+          }
         },
         () => {
+          localStorage.removeItem('authUser');
           this.props.onSetAuthUser(null);
         },
       );
@@ -27,6 +64,10 @@ const withAuthentication = Component => {
     }
   }
 
+  const mapStateToProps = state => ({
+    authUser: state.session.authUser,
+  });
+
   const mapDispatchToProps = dispatch => ({
     onSetAuthUser: authUser => dispatch(setAuthUser(authUser)),
   });
@@ -34,10 +75,15 @@ const withAuthentication = Component => {
   return compose(
     withFirebase,
     connect(
-      null,
+      mapStateToProps,
       mapDispatchToProps,
     ),
-  )(WithAuthentication);
+  )(
+    apolloCompose(
+      graphql(query.createUser, { name: 'createUser' }),
+      graphql(query.fetchUser, { name: 'fetchUser' }),
+    )(WithAuthentication),
+  );
 };
 
 export default withAuthentication;
