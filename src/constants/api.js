b1e0905d9b36ec8ddac6add57bdf011772b3fd04
ozaki <fs65ugumori@yahoo.co.jp>
2018-12-24 23:03:37 +0900

    update api url

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,6 +1,6 @@
 const graphqlServer = process.env.REACT_APP_LOCAL
   ? 'http://localhost:4000/graphql'
-  : 'https://68v4ch48mg.execute-api.ap-northeast-1.amazonaws.com/dev/graphql';
+  : `${process.env.REACT_APP_API_URL}?timestanp=${Date.now()}`;
 
 export default {
   graphqlServer,
