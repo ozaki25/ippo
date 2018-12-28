fa1ebb7980aa0e4805e571ac3c4d907477185adf
ozaki <fs65ugumori@yahoo.co.jp>
2018-12-28 18:09:46 +0900

    replace materialui EventCart component

--- src/components/organisms/EventCard/index.js
+++ src/components/organisms/EventCard/index.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import propTypes from 'prop-types';
-// import { Text } from '@blueprintjs/core';
-// import CustomCard from 'src/components/molecules/CustomCard';
-import { Card, CardActionArea, CardContent, Typography } from '@material-ui/core';
+import CustomCard from 'src/components/molecules/CustomCard';
+import { Typography } from '@material-ui/core';
 
-const EventCard = ({
-  title,
-  eventUrl,
-  catchMessage,
-  place,
-  datetime,
-  interactive,
-  expand,
-  history,
-}) => (
-  <Card expand={expand} interactive={interactive} url={eventUrl} history={history}>
-    <CardActionArea>
-      <CardContent>
-        <h3 className="bp3-heading">{title}</h3>
-        {catchMessage && <Typography>{`概要：${catchMessage}`}</Typography>}
-        {place && <Typography>{`場所：${place}`}</Typography>}
-        {datetime && <Typography>{`日時：${datetime}`}</Typography>}
-      </CardContent>
-    </CardActionArea>
-  </Card>
+const EventCard = ({ title, eventUrl, catchMessage, place, datetime, expand, history }) => (
+  <CustomCard expand={expand} url={eventUrl} history={history}>
+    <h3 className="bp3-heading">{title}</h3>
+    {catchMessage && <Typography>{`概要：${catchMessage}`}</Typography>}
+    {place && <Typography>{`場所：${place}`}</Typography>}
+    {datetime && <Typography>{`日時：${datetime}`}</Typography>}
+  </CustomCard>
 );
 
 EventCard.displayName = 'EventCard';
@@ -34,7 +20,6 @@ EventCard.propTypes = {
   catchMessage: propTypes.string,
   place: propTypes.string,
   datetime: propTypes.string,
-  interactive: propTypes.bool,
   expand: propTypes.bool,
   history: propTypes.shape({
     push: propTypes.func,
@@ -45,7 +30,6 @@ EventCard.defaultProps = {
   catchMessage: '',
   place: '',
   datetime: '',
-  interactive: true,
   expand: false,
   history: null,
 };
