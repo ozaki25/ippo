17dfbd5fd6ec634e4282573c658559cd618aca35
ozaki <fs65ugumori@yahoo.co.jp>
2019-01-01 03:35:31 +0900

    refactor Container and HOC

--- src/components/templates/Container/index.stories.js
+++ src/components/templates/Container/index.stories.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { text } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
 import Container from '.';
 
 const stories = storiesOf('templates/Container', module);
 
 const props = {
+  title: text('title', 'タイトル'),
   children: text('children', 'コンテンツ'),
+  authUser: {
+    uid: '123',
+    displayName: 'テストユーザ',
+  },
+  history: {
+    push: action('push'),
+    goBack: action('goBack'),
+  },
+  firebase: {},
 };
 
 stories.add('paddingあり', () => <Container {...props} />);
 
-stories.add('paddingなし', () => <Container {...props} noPadding={true} />);
+stories.add('paddingなし', () => <Container {...props} noPadding />);
+
+stories.add('戻るあり', () => <Container {...props} back />);
--- src/components/templates/Container/index.test.js
+++ src/components/templates/Container/index.test.js
@@ -4,8 +4,17 @@ import Container from '.';
 
 const props = {
   children: 'テスト',
+  authUser: {
+    uid: '123',
+    displayName: 'テストユーザ',
+  },
+  history: {
+    push: jest.fn(),
+    goBack: jest.fn(),
+  },
+  firebase: {},
 };
 
 snapshot('Container/padding', <Container {...props} />);
 
-snapshot('Container/nopadding', <Container {...props} noPadding={true} />);
+snapshot('Container/nopadding', <Container {...props} noPadding />);
