df399e192077c66d4e883100c547fe405325f172
ozaki <fs65ugumori@yahoo.co.jp>
2019-01-02 00:38:01 +0900

    refactor

--- src/components/atoms/RoundedButton/index.test.js
+++ src/components/atoms/RoundedButton/index.test.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { snapshot } from 'test/helpers';
 import RoundedButton from '.';
 
-const props = () => ({
+const props = {
   children: 'ボタン',
   onClick: jest.fn(),
-});
+};
 
-snapshot('RoundedButton/nomal', <RoundedButton {...props({})} />);
+snapshot('RoundedButton/nomal', <RoundedButton {...props} />);
