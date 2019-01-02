cdc82459377873f46bff34e7205c4a3c4ffcec0c
ozaki <fs65ugumori@yahoo.co.jp>
2019-01-03 02:12:13 +0900

    update EventCreateForm

--- src/components/organisms/EventCreateForm/index.stories.js
+++ src/components/organisms/EventCreateForm/index.stories.js
@@ -6,3 +6,11 @@ import EventCreateForm from '.';
 const stories = storiesOf('organisms/EventCreateForm', module);
 
 stories.add('通常パターン場合', () => <EventCreateForm onSubmit={action('onSubmit')} />);
+
+stories.add('エラー', () => (
+  <EventCreateForm
+    onSubmit={() =>
+      new Promise((resolve, reject) => reject(new Error('TypeError: Failed to fetch')))
+    }
+  />
+));
