844d638090991c06047eabd6eae3bcfd3f2ccd61
ozaki <fs65ugumori@yahoo.co.jp>
2019-01-03 01:16:22 +0900

    update SignupForm component

--- src/components/organisms/SignupForm/index.js
+++ src/components/organisms/SignupForm/index.js
@@ -1,52 +1,75 @@
 import React from 'react';
+import { Button, TextField, Typography } from '@material-ui/core';
 import propTypes from 'prop-types';
-import { Button, FormGroup, InputGroup } from '@blueprintjs/core';
 
 class SignupForm extends React.Component {
-  state = { email: '', pass: '', name: '' };
+  state = { email: '', pass: '', name: '', loading: false, error: '' };
 
-  onClick = event => {
+  onClick = async event => {
     event.preventDefault();
     const { email, pass, name } = this.state;
-    this.props.onSubmit({ data: { email, pass, name } });
+    this.setState({ loading: true });
+    try {
+      await this.props.onSubmit({ data: { email, pass, name } });
+    } catch (e) {
+      this.setState({ error: e.toString() });
+    } finally {
+      this.setState({ loading: false });
+    }
   };
 
   onChange = event => this.setState({ [event.target.name]: event.target.value });
 
   render() {
-    const { email, pass, name } = this.state;
+    const { email, pass, name, loading, error } = this.state;
     const invalid = email.trim() === '' || pass.trim() === '' || name.trim() === '';
     return (
       <form>
-        <FormGroup label="名前" labelFor="name">
-          <InputGroup
-            id="name"
-            name="name"
-            value={this.state.name}
-            onChange={this.onChange}
-            large
-          />
-        </FormGroup>
-        <FormGroup label="メールアドレス" labelFor="email">
-          <InputGroup
-            id="email"
-            name="email"
-            value={this.state.email}
-            onChange={this.onChange}
-            large
-          />
-        </FormGroup>
-        <FormGroup label="パスワード" labelFor="pass">
-          <InputGroup
-            id="pass"
-            name="pass"
-            type="password"
-            value={this.state.pass}
-            onChange={this.onChange}
-            large
-          />
-        </FormGroup>
-        <Button onClick={this.onClick} text="登録" type="submit" disabled={invalid} fill large />
+        {error && <Typography color="error">{error}</Typography>}
+        <TextField
+          label="名前"
+          name="name"
+          value={this.state.name}
+          onChange={this.onChange}
+          margin="normal"
+          color="primary"
+          fullWidth
+          required
+        />
+        <br />
+        <TextField
+          label="メールアドレス"
+          name="email"
+          value={this.state.email}
+          onChange={this.onChange}
+          margin="normal"
+          color="primary"
+          fullWidth
+          required
+        />
+        <br />
+        <TextField
+          label="パスワード"
+          name="pass"
+          type="password"
+          value={this.state.pass}
+          onChange={this.onChange}
+          margin="normal"
+          color="primary"
+          fullWidth
+          required
+        />
+        <br />
+        <Button
+          onClick={this.onClick}
+          type="submit"
+          disabled={invalid || loading}
+          color="primary"
+          variant="contained"
+          fullWidth
+        >
+          登録
+        </Button>
       </form>
     );
   }
--- src/components/organisms/SignupForm/index.stories.js
+++ src/components/organisms/SignupForm/index.stories.js
@@ -5,6 +5,14 @@ import SignupForm from '.';
 
 const stories = storiesOf('organisms/SignupForm', module);
 
-stories.add('通常パターン', () => (
-  <SignupForm onSubmit={action('onSubmit')}  />
+stories.add('通常パターン', () => <SignupForm onSubmit={action('onSubmit')} />);
+
+stories.add('エラー', () => (
+  <SignupForm
+    onSubmit={() =>
+      new Promise((resolve, reject) =>
+        reject(new Error('The email address is already in use by another account.')),
+      )
+    }
+  />
 ));
