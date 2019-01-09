a7ee164ee4e1206105447acd26350f48e93f42a1
ozaki <fs65ugumori@yahoo.co.jp>
2019-01-10 08:35:56 +0900

    add settings notification look only

--- src/components/pages/SettingsNotification/index.js
+++ src/components/pages/SettingsNotification/index.js
@@ -1,15 +1,41 @@
 import React from 'react';
+import { List, ListItem, ListItemSecondaryAction, ListItemText, Switch } from '@material-ui/core';
 import propTypes from 'prop-types';
 import Container from 'src/components/templates/Container';
 
-const SettingsNotification = ({ createEvent, authUser, history, firebase }) => {
-  return (
-    <Container title="プッシュ通知" back authUser={authUser} history={history} firebase={firebase}>
-      <p />
-    </Container>
-  );
-};
+class SettingsNotification extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { checked: false };
+  }
 
+  toggleNotificationPermission = () =>
+    this.setState(prevState => ({ checked: !prevState.checked }));
+
+  render() {
+    const { authUser, history, firebase } = this.props;
+    const { checked } = this.state;
+    return (
+      <Container title="通知設定" back authUser={authUser} history={history} firebase={firebase}>
+        <List>
+          <ListItem>
+            <ListItemText
+              primary="プッシュ通知"
+              secondary="おすすめのイベントや参加イベントのリマインドの通知を受け取ることができます"
+            />
+            <ListItemSecondaryAction>
+              <Switch
+                onChange={this.toggleNotificationPermission}
+                checked={checked}
+                color="primary"
+              />
+            </ListItemSecondaryAction>
+          </ListItem>
+        </List>
+      </Container>
+    );
+  }
+}
 SettingsNotification.displayName = 'SettingsNotification';
 
 SettingsNotification.propTypes = {
