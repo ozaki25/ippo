71b36f3176d69ebc536c2fb6ef1634b93136452a
ozaki <fs65ugumori@yahoo.co.jp>
2019-01-11 21:26:46 +0900

    add SettingsNotification story

--- src/components/pages/SettingsNotification/index.stories.js
+++ src/components/pages/SettingsNotification/index.stories.js
@@ -5,7 +5,7 @@ import SettingsNotification from '.';
 
 const stories = storiesOf('pages/SettingsNotification', module);
 
-const props = ({ granted, supported }) => ({
+const props = ({ granted, denied, supported }) => ({
   authUser: {
     uid: '123',
     displayName: 'テストユーザ',
@@ -15,21 +15,30 @@ const props = ({ granted, supported }) => ({
     goBack: action('goBack'),
     replace: action('replace'),
   },
-  firebase: {},
+  firebase: {
+    askForPermissionToReceiveNotifications: action('askForPermissionToReceiveNotifications'),
+  },
   notifications: {
     isGranted: () => granted,
+    isDenied: () => denied,
     isSupported: () => supported,
   },
+  registerNotification: action('register'),
+  unregisterNotification: action('unregister'),
 });
 
 stories.add('通知未対応端末', () => (
-  <SettingsNotification {...props({ granted: false, supported: false })} />
+  <SettingsNotification {...props({ granted: false, denied: false, supported: false })} />
 ));
 
-stories.add('通知対応端末で未許可', () => (
-  <SettingsNotification {...props({ granted: false, supported: true })} />
+stories.add('通知対応端末で未設定', () => (
+  <SettingsNotification {...props({ granted: false, denied: false, supported: true })} />
 ));
 
 stories.add('通知対応端末で許可済み', () => (
-  <SettingsNotification {...props({ granted: true, supported: true })} />
+  <SettingsNotification {...props({ granted: true, denied: false, supported: true })} />
+));
+
+stories.add('通知対応端末で拒否済み', () => (
+  <SettingsNotification {...props({ granted: false, denied: true, supported: true })} />
 ));
