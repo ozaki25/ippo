0814d4bdc61efac385468687a36af70be290df69
ozaki25 <fs65ugumori@yahoo.co.jp>
2019-01-16 11:05:37 +0900

    connect fetUser query to SettingsAccount

--- src/containers/SettingsAccount.js
+++ src/containers/SettingsAccount.js
@@ -1,5 +1,7 @@
+import { graphql } from 'react-apollo';
 import { compose } from 'recompose';
 import { withRouter } from 'react-router-dom';
+import query from 'src/graphql/query';
 import SettingsAccount from 'src/components/pages/SettingsAccount';
 import { withFirebase } from 'src/context/firebase';
 import { withAuthorization } from 'src/hoc/Sessions';
@@ -8,4 +10,7 @@ export default compose(
   withAuthorization,
   withRouter,
   withFirebase,
+  graphql(query.fetchUser, {
+    options: ({ authUser: { uid } }) => ({ variables: { uid } }),
+  }),
 )(SettingsAccount);
