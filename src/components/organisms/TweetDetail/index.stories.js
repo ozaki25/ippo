ef4f863c78ae5f80c29014c0b5416ac0788fee6d
ozaki <fs65ugumori@yahoo.co.jp>
2019-01-20 01:28:13 +0900

    add TweetDetail component

---
+++ src/components/organisms/TweetDetail/index.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { text } from '@storybook/addon-knobs';
+import TweetDetail from '.';
+
+const stories = storiesOf('organisms/TweetDetail', module);
+
+const props = {
+  name: text('name', 'ozaki25'),
+  text: text(
+    'text',
+    `Storybookいいところ
+  - どんなコンポーネントが存在してるか可視化できる
+  - コンポーネントの扱い方が分かる
+  - UIの修正デバッグが楽
+  #ThinkAtomicDesign`,
+  ),
+  time: text('time', '2019/01/19 01:44'),
+};
+
+stories.add('通常パターン', () => <TweetDetail {...props} />);
---
+++ src/components/organisms/TweetDetail/index.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { snapshot } from 'test/helpers';
+import Tweet from '.';
+
+const props = {
+  name: 'ozaki25',
+  text: `Storybookいいところ
+  - どんなコンポーネントが存在してるか可視化できる
+  - コンポーネントの扱い方が分かる
+  - UIの修正デバッグが楽
+  #ThinkAtomicDesign`,
+  time: '2019/01/19 01:44',
+};
+
+snapshot('Tweet/nomal', <Tweet {...props} />);
