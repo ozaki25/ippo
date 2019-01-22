d8a9439975bbbe80dd5effe5978796a9ce7dbe16
ozaki <fs65ugumori@yahoo.co.jp>
2019-01-23 02:04:40 +0900

    A2HS description dialog

---
+++ src/components/organisms/A2HSDialog/index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Button, Dialog, DialogContent, DialogTitle, Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import styled from 'styled-components';
+import propTypes from 'prop-types';
+
+const StyledImage = styled.img`
+  max-width: 100%;
+`;
+
+const IosA2HS = () => (
+  <>
+    <Typography>1.以下のアイコンを選択</Typography>
+    <StyledImage src="/images/a2hs/ios1.png" alt="ios1" />
+    <Typography>2.「ホーム画面に追加」を選択</Typography>
+    <StyledImage src="/images/a2hs/ios2.png" alt="ios2" />
+    <Typography>3.「追加」を選択</Typography>
+    <StyledImage src="/images/a2hs/ios3.png" alt="ios3" />
+    <Typography>4.ホーム画面にアプリがインスールされる</Typography>
+    <StyledImage src="/images/a2hs/ios4.png" alt="ios4" />
+  </>
+);
+
+const AndroidA2HS = () => (
+  <>
+    <Typography>1.以下のボタンから設定を開く</Typography>
+    <StyledImage src="/images/a2hs/android1.jpeg" alt="ios1" />
+    <Typography>2.「ホーム画面に追加」を選択</Typography>
+    <StyledImage src="/images/a2hs/android2.jpeg" alt="android2" />
+    <Typography>3.「追加」を選択</Typography>
+    <StyledImage src="/images/a2hs/android3.jpeg" alt="android3" />
+    <Typography>4.ホーム画面にアプリがインスールされる</Typography>
+    <StyledImage src="/images/a2hs/android4.jpeg" alt="android4" />
+  </>
+);
+
+const styles = {
+  nonCaps: {
+    textTransform: 'none',
+  },
+};
+
+class A2HSDialog extends React.Component {
+  state = { content: '' };
+
+  render() {
+    const { open, onClose, classes } = this.props;
+    const { content } = this.state;
+    return (
+      <Dialog open={open} onClose={onClose}>
+        <DialogTitle id="simple-dialog-title">ホーム画面への追加手順</DialogTitle>
+        <DialogContent>
+          <>
+            <Button className={classes.nonCaps} onClick={() => this.setState({ content: '1' })}>
+              iOS/Safariをお使いの方
+            </Button>
+            <Button className={classes.nonCaps} onClick={() => this.setState({ content: '2' })}>
+              Android/Chromeをお使いの方
+            </Button>
+            {content === '1' && <IosA2HS />}
+            {content === '2' && <AndroidA2HS />}
+          </>
+        </DialogContent>
+      </Dialog>
+    );
+  }
+}
+
+A2HSDialog.displayName = 'A2HSDialog';
+
+A2HSDialog.propTypes = {
+  open: propTypes.bool.isRequired,
+  onClose: propTypes.func.isRequired,
+};
+
+A2HSDialog.defaultProps = {};
+
+export default withStyles(styles)(A2HSDialog);
--- src/components/organisms/SideMenu/index.js
+++ src/components/organisms/SideMenu/index.js
@@ -12,6 +12,7 @@ import {
   AccountBox,
   AddBoxRounded,
   ExitToApp,
+  GetAppRounded,
   HomeRounded,
   NotificationsRounded,
   NotificationsNoneRounded,
@@ -19,6 +20,7 @@ import {
 import { withStyles } from '@material-ui/core/styles';
 import propTypes from 'prop-types';
 import CharIcon from 'src/components/atoms/CharIcon';
+import A2HSDialog from 'src/components/organisms/A2HSDialog';
 import ROUTES from 'src/constants/routes';
 import MENU_ITEMS from 'src/constants/menuItems';
 
@@ -28,75 +30,101 @@ const styles = {
   },
 };
 
-const SideMenu = ({ open, name, onOpen, onClose, signout, history, classes }) => (
-  <SwipeableDrawer anchor="right" open={open} onClose={onClose} onOpen={onOpen}>
-    <div tabIndex={0} onClick={onClose} onKeyDown={onClose}>
-      <div className={classes.list}>
-        <List>
-          <ListItem>
-            <ListItemIcon>
-              <CharIcon name={name} />
-            </ListItemIcon>
-          </ListItem>
-          <ListItem>
-            <ListItemText primary={name} />
-          </ListItem>
-        </List>
-        <Divider />
-        <List subheader={<ListSubheader>メニュー</ListSubheader>}>
-          <ListItem onClick={() => history.push(ROUTES.Menu)} button>
-            <ListItemIcon>
-              <HomeRounded />
-            </ListItemIcon>
-            <ListItemText primary="ホーム" />
-          </ListItem>
-          <ListItem
-            onClick={() => history.push(`${ROUTES.Menu}?tab=${MENU_ITEMS.NEW_EVENT.title}`)}
-            button
-          >
-            <ListItemIcon>
-              <AddBoxRounded />
-            </ListItemIcon>
-            <ListItemText primary="イベント作成" />
-          </ListItem>
-          <ListItem
-            onClick={() => history.push(`${ROUTES.Menu}?tab=${MENU_ITEMS.NOTIFICATION.title}`)}
-            button
-          >
-            <ListItemIcon>
-              <NotificationsRounded />
-            </ListItemIcon>
-            <ListItemText primary="通知一覧" />
-          </ListItem>
-        </List>
-        <Divider />
-        <List subheader={<ListSubheader>設定</ListSubheader>}>
-          <ListItem onClick={() => history.push(ROUTES.SettingsAccount)} button>
-            <ListItemIcon>
-              <AccountBox />
-            </ListItemIcon>
-            <ListItemText primary="アカウント設定" />
-          </ListItem>
-          <ListItem onClick={() => history.push(ROUTES.SettingsNotification)} button>
-            <ListItemIcon>
-              <NotificationsNoneRounded />
-            </ListItemIcon>
-            <ListItemText primary="通知設定" />
-          </ListItem>
-        </List>
-        <Divider />
-        <List>
-          <ListItem onClick={signout} button>
-            <ListItemIcon>
-              <ExitToApp />
-            </ListItemIcon>
-            <ListItemText primary="ログアウト" />
-          </ListItem>
-        </List>
-      </div>
-    </div>
-  </SwipeableDrawer>
-);
+class SideMenu extends React.Component {
+  state = { isOpenA2HSDialog: false };
+
+  openA2HSDialog = () => this.setState({ isOpenA2HSDialog: true, open: false });
+
+  closeA2HSDialog = () => this.setState({ isOpenA2HSDialog: false });
+
+  render() {
+    const { open, name, onOpen, onClose, signout, history, classes } = this.props;
+    const { isOpenA2HSDialog } = this.state;
+    return (
+      <>
+        <SwipeableDrawer anchor="right" open={open} onClose={onClose} onOpen={onOpen}>
+          <div tabIndex={0} onClick={onClose} onKeyDown={onClose}>
+            <div className={classes.list}>
+              <List>
+                <ListItem>
+                  <ListItemIcon>
+                    <CharIcon name={name} />
+                  </ListItemIcon>
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary={name} />
+                </ListItem>
+              </List>
+              <Divider />
+              <List subheader={<ListSubheader>メニュー</ListSubheader>}>
+                <ListItem onClick={() => history.push(ROUTES.Menu)} button>
+                  <ListItemIcon>
+                    <HomeRounded />
+                  </ListItemIcon>
+                  <ListItemText primary="ホーム" />
+                </ListItem>
+                <ListItem
+                  onClick={() => history.push(`${ROUTES.Menu}?tab=${MENU_ITEMS.NEW_EVENT.title}`)}
+                  button
+                >
+                  <ListItemIcon>
+                    <AddBoxRounded />
+                  </ListItemIcon>
+                  <ListItemText primary="イベント作成" />
+                </ListItem>
+                <ListItem
+                  onClick={() =>
+                    history.push(`${ROUTES.Menu}?tab=${MENU_ITEMS.NOTIFICATION.title}`)
+                  }
+                  button
+                >
+                  <ListItemIcon>
+                    <NotificationsRounded />
+                  </ListItemIcon>
+                  <ListItemText primary="通知一覧" />
+                </ListItem>
+              </List>
+              <Divider />
+              <List subheader={<ListSubheader>設定</ListSubheader>}>
+                <ListItem onClick={() => history.push(ROUTES.SettingsAccount)} button>
+                  <ListItemIcon>
+                    <AccountBox />
+                  </ListItemIcon>
+                  <ListItemText primary="アカウント設定" />
+                </ListItem>
+                <ListItem onClick={() => history.push(ROUTES.SettingsNotification)} button>
+                  <ListItemIcon>
+                    <NotificationsNoneRounded />
+                  </ListItemIcon>
+                  <ListItemText primary="通知設定" />
+                </ListItem>
+              </List>
+              <Divider />
+              <List subheader={<ListSubheader>その他</ListSubheader>}>
+                <ListItem onClick={this.openA2HSDialog} button>
+                  <ListItemIcon>
+                    <GetAppRounded />
+                  </ListItemIcon>
+                  <ListItemText primary="ホーム画面に追加" />
+                </ListItem>
+              </List>
+              <Divider />
+              <List>
+                <ListItem onClick={signout} button>
+                  <ListItemIcon>
+                    <ExitToApp />
+                  </ListItemIcon>
+                  <ListItemText primary="ログアウト" />
+                </ListItem>
+              </List>
+            </div>
+          </div>
+        </SwipeableDrawer>
+        <A2HSDialog open={isOpenA2HSDialog} onClose={this.closeA2HSDialog} />
+      </>
+    );
+  }
+}
 
 SideMenu.displayName = 'SideMenu';
 
