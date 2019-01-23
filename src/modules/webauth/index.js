53d0186fcf422309a4733146f4b9a5a82f8dd0b2
ozaki <fs65ugumori@yahoo.co.jp>
2019-01-23 20:08:36 +0900

    modify redux

--- src/modules/webauth/index.js
+++ src/modules/webauth/index.js
@@ -1,18 +1,20 @@
 const WEB_AUTH_ADD = 'WEB_AUTH/ADD';
 
-export function setRawId(rawId) {
-  return { type: WEB_AUTH_ADD, rawId };
+export function setRawId({ rawId, uid }) {
+  return { type: WEB_AUTH_ADD, body: { rawId, uid } };
 }
 
 const INITIAL_STATE = {
   rawId: null,
+  uid: null,
 };
 
 function reducer(state = INITIAL_STATE, action) {
-  const { type, rawId } = action;
+  const { type, body } = action;
   switch (type) {
     case WEB_AUTH_ADD: {
-      return { ...state, rawId };
+      const { rawId, uid } = body;
+      return { ...state, rawId, uid };
     }
     default:
       return state;
