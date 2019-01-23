afb934b35c4ee670a741607d479024c84d8d6493
ozaki25 <fs65ugumori@yahoo.co.jp>
2019-01-23 21:21:17 +0900

    Update index.js

--- src/components/organisms/TweetDetail/index.js
+++ src/components/organisms/TweetDetail/index.js
@@ -9,7 +9,7 @@ import TweetFooter from 'src/components/organisms/TweetFooter';
 import dateFormat from 'src/utils/dateFormat';
 
 const Container = styled.div`
-  padding: 3px 0 250px;
+  padding-bottom: 300px;
 `;
 
 const TweetContainer = styled.div`
