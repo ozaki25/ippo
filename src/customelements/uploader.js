7e83dbafb584cee0d332ca386b0f33679709dd53
ozaki <fs65ugumori@yahoo.co.jp>
2019-01-26 00:44:50 +0900

    update uploader custom elements

--- src/customelements/uploader.js
+++ src/customelements/uploader.js
@@ -1,39 +1,57 @@
+import api from 'src/constants/api';
+
+const lambdaUrl = api.uploadEndpoint;
+
 const template = `
-<form
-  action="${process.env.REACT_APP_S3_URL}"
-  method="post"
-  enctype="multipart/form-data"
->
-  <input type="hidden" name="key" value="" />
-  <input type="hidden" name="AWSAccessKeyId" value="${
-    process.env.REACT_APP_UPLOADER_AWS_ACCESS_KEY_ID
-  }" />
-  <input type="hidden" name="acl" value="private" />
-  <input type="hidden" name="success_action_redirect" value="https://www.google.co.jp" />
-  <input type="hidden" name="policy" value="${process.env.REACT_APP_POLICY}"  />
-  <input type="hidden" name="signature" value="${process.env.REACT_APP_SIGNATURE}" />
-  <input type="hidden" name="Content-Type" value="" />
+<div>
   <input type="file" name="file" />
-  <input type="submit" name="button" value="Upload" />
-</form>
+  <button type="button">Upload</button>
+</div>
 `;
 
 class XUploader extends HTMLElement {
   connectedCallback() {
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.innerHTML = template;
-    this.shadowRoot.querySelector('form').addEventListener('submit', this.onSubmit);
+    this.shadowRoot.querySelector('button').addEventListener('click', this.onClick);
   }
 
-  onSubmit(e) {
-    const { target } = e;
+  getUploadUrl = ({ url, filename, mimetype }) =>
+    fetch(`${url}?filename=${filename}&mimetype=${mimetype}`);
+
+  putFile = ({ url, file, type }) =>
+    fetch(url, { method: 'PUT', headers: { 'Content-Type': type }, body: file });
+
+  dispatch = ({ url, name, error }) => {
+    const event = new CustomEvent('uploaded', { detail: { url, name, error } });
+    window.dispatchEvent(event);
+  };
+
+  onClick = async e => {
     e.preventDefault();
-    const key = target.querySelector('input[name="key"]');
-    const inputFile = target.querySelector('input[name="file"]');
+    const inputFile = this.shadowRoot.querySelector('input[name="file"]');
     const file = inputFile.files[0];
-    key.setAttribute('value', `uploads/${file.name}`);
-    target.submit();
-  }
+    const { name, type } = file;
+
+    try {
+      const getUploadUrlResponse = await this.getUploadUrl({
+        url: lambdaUrl,
+        filename: name,
+        mimetype: type,
+      });
+      const { url, bucket } = await getUploadUrlResponse.json();
+      const putFileResponse = await this.putFile({ url, file, type });
+      if (putFileResponse.ok) {
+        this.dispatch({ url: `https://s3-ap-northeast-1.amazonaws.com/${bucket}/${name}`, name });
+      } else {
+        console.log(putFileResponse);
+        this.onError({ error: 'Failed' });
+      }
+    } catch (e) {
+      console.log(e);
+      this.onError({ error: e });
+    }
+  };
 }
 
 window.customElements.define('x-uploader', XUploader);
