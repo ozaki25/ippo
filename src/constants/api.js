4a5d3a10b226acc42792e53ed221ff11544e64d0
ozaki <fs65ugumori@yahoo.co.jp>
2019-01-26 00:44:34 +0900

    add get s3 upload url endpoint

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,7 +1,9 @@
 const graphqlServer = process.env.REACT_APP_LOCAL
   ? 'http://localhost:4000/graphql'
   : `${process.env.REACT_APP_API_URL}`;
+const uploadEndpoint = process.env.REACT_APP_GET_FILE_UPLOAD_ENDPOINT;
 
 export default {
   graphqlServer,
+  uploadEndpoint,
 };
