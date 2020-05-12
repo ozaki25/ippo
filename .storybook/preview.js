fc8cd944a01203183851ceaf1538efcacf173f74
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-05-12 16:25:12 +0900

    update storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,10 @@
 import { addDecorator, addParameters } from '@storybook/react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
-import 'typeface-roboto';
+import StoryRouter from 'storybook-react-router';
 import Layout from './Layout';
-import '../src/index.css';
 
 addDecorator(Layout);
+addDecorator(StoryRouter());
 
 addParameters({
   viewport: {
