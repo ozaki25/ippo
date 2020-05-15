a0ed2e8063cc9a8ccf0c0800ba80e2af6d233874
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-05-15 17:46:49 +0900

    Tweet component apollo/hoc to apollo/client

--- src/containers/Tweet.js
+++ src/containers/Tweet.js
@@ -1,23 +1,41 @@
-import { graphql } from '@apollo/react-hoc';
+import React from 'react';
+import { useQuery, useMutation } from '@apollo/client';
 import { compose } from 'recompose';
 import { withRouter } from 'react-router-dom';
+
 import { withFirebase } from 'src/context/firebase';
+import { withAuthorization } from 'src/hoc/Sessions';
 import query from 'src/graphql/query';
 import mutation from 'src/graphql/mutation';
 import Tweet from 'src/components/pages/Tweet';
-import { withAuthorization } from 'src/hoc/Sessions';
 
 const getHashtag = search =>
   new URLSearchParams(search).get('hashtag') || 'none';
 
-export default compose(
+const WithTweet = compose(
   withAuthorization,
   withRouter,
   withFirebase,
-  graphql(query.tweet, {
-    options: ({ match: { params }, location: { search } }) => ({
-      variables: { hashtag: getHashtag(search), id: params.id },
-    }),
-  }),
-  graphql(mutation.addLikeToTweet, { name: 'addLike' }),
-)(Tweet);
+)(TweetContainer);
+
+function TweetContainer(props) {
+  const {
+    match: {
+      params: { id },
+    },
+    location: { search },
+  } = props;
+  const { data, loading, error, refetch, variables } = useQuery(query.tweet, {
+    variables: { hashtag: getHashtag(search), id },
+  });
+  const [addLike] = useMutation(mutation.addLikeToTweet);
+  return (
+    <Tweet
+      {...props}
+      addLike={addLike}
+      data={{ ...data, loading, error, refetch, variables }}
+    />
+  );
+}
+
+export default WithTweet;
