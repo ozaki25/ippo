9d5bb3660aed82d9773dd9ceb959f574b729f3f0
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-05-15 16:59:19 +0900

    NewEvent component apollo/hoc to apollo/client

--- src/containers/NewEvent.js
+++ src/containers/NewEvent.js
@@ -1,14 +1,22 @@
-import { graphql } from '@apollo/react-hoc';
+import React from 'react';
+import { useMutation } from '@apollo/client';
 import { compose } from 'recompose';
 import { withRouter } from 'react-router-dom';
+
 import { withFirebase } from 'src/context/firebase';
+import { withAuthorization } from 'src/hoc/Sessions';
 import mutation from 'src/graphql/mutation';
 import NewEvent from 'src/components/pages/NewEvent/';
-import { withAuthorization } from 'src/hoc/Sessions';
 
-export default compose(
+const WithNewEvent = compose(
   withAuthorization,
   withRouter,
   withFirebase,
-  graphql(mutation.createEvent, { name: 'createEvent' }),
 )(NewEvent);
+
+function NewEventContainer() {
+  const [createEvent] = useMutation(mutation.createEvent);
+  return <WithNewEvent createEvent={createEvent} />;
+}
+
+export default NewEventContainer;
