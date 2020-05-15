41b7fc7c2244dbe62e48f37361505ff77f2a98d3
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-05-15 17:42:23 +0900

    OrganizedEvent component apollo/hoc to apollo/client

--- src/containers/OrganizedEvents.js
+++ src/containers/OrganizedEvents.js
@@ -1,19 +1,28 @@
-import { graphql } from '@apollo/react-hoc';
+import React from 'react';
+import { useQuery } from '@apollo/client';
 import { compose } from 'recompose';
 import { withRouter } from 'react-router-dom';
+
 import { withFirebase } from 'src/context/firebase';
-import query from 'src/graphql/query';
-import OrganizedEvents from 'src/components/pages/OrganizedEvents/';
 import { withAuthorization } from 'src/hoc/Sessions';
 import paging from 'src/constants/paging';
+import query from 'src/graphql/query';
+import OrganizedEvents from 'src/components/pages/OrganizedEvents/';
 
-export default compose(
+const WithOrganizedEvents = compose(
   withAuthorization,
   withRouter,
   withFirebase,
-  graphql(query.organizedEvents, {
-    options: ({ authUser: { uid } }) => ({
-      variables: { uid, limit: paging.eventsPerPage },
-    }),
-  }),
-)(OrganizedEvents);
+)(OrganizedEventsContainer);
+
+function OrganizedEventsContainer(props) {
+  const { uid } = props.authUser;
+  const { data, loading, error, fetchMore } = useQuery(query.organizedEvents, {
+    variables: { uid, limit: paging.eventsPerPage },
+  });
+  return (
+    <OrganizedEvents {...props} data={{ ...data, loading, error, fetchMore }} />
+  );
+}
+
+export default WithOrganizedEvents;
