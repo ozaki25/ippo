cc37d4e7becc49b6c8a390586500f1ac65208c0d
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-05-15 16:59:55 +0900

    Notification component apollo/hoc to apollo/client

--- src/containers/Notification.js
+++ src/containers/Notification.js
@@ -1,19 +1,36 @@
-import { graphql } from '@apollo/react-hoc';
+import React from 'react';
+import { useQuery, useMutation } from '@apollo/client';
 import { compose } from 'recompose';
 import { withRouter } from 'react-router-dom';
+
 import { withFirebase } from 'src/context/firebase';
+import { withAuthorization } from 'src/hoc/Sessions';
 import query from 'src/graphql/query';
 import mutation from 'src/graphql/mutation';
 import Notification from 'src/components/pages/Notification';
-import { withAuthorization } from 'src/hoc/Sessions';
 
-export default compose(
+const WithNotification = compose(
   withAuthorization,
   withRouter,
   withFirebase,
-  graphql(query.fetchUser, {
-    options: ({ authUser: { uid } }) => ({ variables: { uid } }),
-    name: 'user',
-  }),
-  graphql(mutation.readNotification, { name: 'readNotification' }),
-)(Notification);
+)(NotificationContainer);
+
+function NotificationContainer(props) {
+  const { uid } = props.authUser;
+  const { data, loading, error, refetch, variables } = useQuery(
+    query.fetchUser,
+    {
+      variables: { uid },
+    },
+  );
+  const [readNotification] = useMutation(mutation.readNotification);
+  return (
+    <Notification
+      {...props}
+      readNotification={readNotification}
+      user={{ ...data, loading, error, refetch, variables }}
+    />
+  );
+}
+
+export default WithNotification;
