094c9a24e7842b38ea124b6df94ccfca9065ab91
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-05-15 17:45:50 +0900

    SettingsNotification component apollo/hoc to apollo/client

--- src/containers/SettingsNotification.js
+++ src/containers/SettingsNotification.js
@@ -1,17 +1,30 @@
+import React from 'react';
+import { useMutation } from '@apollo/client';
 import { compose } from 'recompose';
 import { withRouter } from 'react-router-dom';
-import { graphql } from '@apollo/react-hoc';
-import mutation from 'src/graphql/mutation';
-import SettingsNotification from 'src/components/pages/SettingsNotification/';
+
 import { withFirebase } from 'src/context/firebase';
 import { withAuthorization } from 'src/hoc/Sessions';
 import withNotification from 'src/hoc/withNotification';
+import mutation from 'src/graphql/mutation';
+import SettingsNotification from 'src/components/pages/SettingsNotification/';
 
-export default compose(
+const WithSettingsNotification = compose(
   withAuthorization,
   withRouter,
   withFirebase,
   withNotification,
-  graphql(mutation.registerNotification, { name: 'registerNotification' }),
-  graphql(mutation.unregisterNotification, { name: 'unregisterNotification' }),
 )(SettingsNotification);
+
+function SettingsNotificationContainer() {
+  const [registerNotification] = useMutation(mutation.registerNotification);
+  const [unregisterNotification] = useMutation(mutation.unregisterNotification);
+  return (
+    <WithSettingsNotification
+      registerNotification={registerNotification}
+      unregisterNotification={unregisterNotification}
+    />
+  );
+}
+
+export default SettingsNotificationContainer;
