92de8e39d1933a2e864c368c1d4e55abd1b119a7
ozaki <fs65ugumori@yahoo.co.jp>
2020-05-29 01:11:28 +0900

    update firebase context

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { MuiThemeProvider } from '@material-ui/core/styles';
 import Router from 'src/router';
 import client from 'src/graphql/client';
 import store from 'src/modules/createStore';
-import Firebase from 'src/utils/firebase';
 import { FirebaseProvider } from 'src/context/firebase';
 import theme from 'src/theme';
 import { withAuthentication } from 'src/hoc/Sessions';
@@ -17,7 +16,7 @@ const App = () => (
   <MuiThemeProvider theme={theme}>
     <ApolloProvider client={client}>
       <Provider store={store}>
-        <FirebaseProvider value={new Firebase()}>
+        <FirebaseProvider>
           <AuthRouter />
         </FirebaseProvider>
       </Provider>
