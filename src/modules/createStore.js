f1decc90847a8631ce5a9b671b3d8735c5e65392
ozaki <fs65ugumori@yahoo.co.jp>
2020-05-29 01:36:03 +0900

    remove redux modules

--- src/modules/createStore.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux';
-import reducers from 'src/modules/reducers';
-
-const store = createStore(reducers);
-
-export default store;
--- src/modules/reducers.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import session from 'src/modules/session';
-
-const reducer = combineReducers({ session });
-
-export default reducer;
